0c1f5ed8710c9eaddd302484af98c30e4667630a
Nazik007 <99899134+Nazik007@users.noreply.github.com>
2022-05-31 10:30:32 +0300

    Create shapetest.java

---
+++ shapetest.java
@@ -0,0 +1,18 @@
+public class ShapeTest {
+
+	public static void main(String[] args) {
+		ShapePrinter instance = new ShapePrinter();
+		
+		Shape square = new Square("Square Gimli", 4);
+		Rectangle rectangle = new Rectangle("Rectangle Hancock", 12, 8);
+		Triangle triangle = new Triangle("Triangle Joshua", 10, 5);
+		Circle circle = new Circle("Circle Halyna", 5);
+		Trapezoid trapezoid = new Trapezoid("Trapezoid Rex", 8, 10, 6);
+
+		instance.printer(square);
+		instance.printer(rectangle);
+		instance.printer(triangle);
+		instance.printer(circle);
+		instance.printer(trapezoid);
+	}
+}
