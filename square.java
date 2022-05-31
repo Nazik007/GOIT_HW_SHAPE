a6377b9d306b110ee6f89e7fa21b9557ca696fa0
Nazik007 <99899134+Nazik007@users.noreply.github.com>
2022-05-31 10:32:03 +0300

    Create square.java

---
+++ square.java
@@ -0,0 +1,27 @@
+public class Square extends Shape {
+	private String shapeName;
+	private int side;
+
+	public Square(String shapeName, int side) {
+		this.shapeName = shapeName;
+		this.side = side;
+	}
+
+	public String getshapeName() {
+		return shapeName;
+	}
+
+	public int getSide() {
+		return side;
+	}
+
+	@Override
+	public String getName() {
+		return "My name is : " + getshapeName();
+	}
+
+	@Override
+	public String getArea() {
+		return "And my area is : " + (double)(getSide() * getSide());
+	}
+}
