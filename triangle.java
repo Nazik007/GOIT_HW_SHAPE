ece02e8768b456619429f4973fdeab66b0ef5fd3
Nazik007 <99899134+Nazik007@users.noreply.github.com>
2022-05-31 10:33:20 +0300

    Create triangle.java

---
+++ triangle.java
@@ -0,0 +1,34 @@
+public class Triangle extends Shape {
+	private String shapeName;
+	private int height;
+	private int base;
+
+	public Triangle(String shapeName, int height, int base) {
+		this.shapeName = shapeName;
+		this.height = height;
+		this.base = base;
+	}
+
+	public String getshapeName() {
+		return shapeName;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public double getBase() {
+		return base;
+	}
+
+	@Override
+	public String getName() {
+		return "My name is : " + getshapeName();
+	}
+
+	@Override
+	public String getArea() {
+		return "And my area is : " + (double)((getHeight() * getBase()) / 2);
+		
+	}
+}
