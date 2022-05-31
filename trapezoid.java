a2d9b059a3e6e174f6bcfef472e25ae917989350
Nazik007 <99899134+Nazik007@users.noreply.github.com>
2022-05-31 10:32:42 +0300

    Create trapezoid.java

---
+++ trapezoid.java
@@ -0,0 +1,40 @@
+public class Trapezoid extends Shape {
+	private String shapeName;
+	private int side1;
+	private int side2;
+	private int height;
+	
+	public Trapezoid(String shapeName, int side1, int side2, int height) {
+		this.shapeName = shapeName;
+		this.side1 = side1;
+		this.side2 = side2;
+		this.height = height;
+	}
+
+	public String getshapeName() {
+		return shapeName;
+	}
+
+	public int getSide1() {
+		return side1;
+	}
+
+	public int getSide2() {
+		return side2;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	@Override
+	public String getName() {
+		return "My name is : " + getshapeName();
+	}
+
+	@Override
+	public String getArea() {
+		return "My area is : " + (double)(((getSide1() + getSide2()) / 2) * getHeight());
+	}
+
+}
